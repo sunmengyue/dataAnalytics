0d9e779a36bbb1c5308ef0d798b7607c0a84b184
Simon Sun <bardrabbit@gmail.com>
2018-09-29 13:55:04 -0400

    Add mean absolute deviation(MAD) for the trendline model

--- linearRegressionTrendline.R
+++ linearRegressionTrendline.R
@@ -14,3 +14,16 @@ summary(model)
 print(model)
 # F.level =  625.5549  +  (-0.0242) * year
 # F(1973) =  625.5549  +  (-0.0242) * 1973
+F.t <- 625.5549  +  (-0.0242) * 1973
+F.t
+
+#MAD (Mean Absolute Deviation) calculation in R
+df$F <- 0
+df$absErr <- 0
+for (i in 1:nrow(df)) {
+  df$F[i] <- -0.024 * df$year[i] + 625.5549
+  df$absErr[i] <- abs(df[i, 2] - df$F[i])
+}
+head(df)
+mean(df$absErr)
+# Mad(trendline) is 0.91
